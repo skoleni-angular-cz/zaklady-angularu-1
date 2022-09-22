44b3d70c5b11fafdf90aca2f77b60394249fd7f4
skoleni-angular-cz <114166795+skoleni-angular-cz@users.noreply.github.com>
2022-09-22 17:31:23 +0200

    2 - Implemented molecule components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todo.model';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  todoItem: TodoItem = {
+    id: 1,
+    title: 'Buy milk',
+    completed: false,
+  };
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MyButtonComponent } from './my-button/my-button.component';
 import { MyTextFieldComponent } from './my-text-field/my-text-field.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, MyButtonComponent, MyTextFieldComponent],
+  declarations: [
+    AppComponent,
+    MyButtonComponent,
+    MyTextFieldComponent,
+    TodoItemComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
